e6240b80b0b183b93ba4ec6b5f9a9c4b93589af3
yukku0423 <yukku0423@gmail.com>
2022-02-12 21:40:42 +0900

    update

--- src/resources/data.ts
+++ src/resources/data.ts
@@ -1,58 +1,209 @@
 export default [
   {
-    id: 1,
-    cat: 1,
+    id: 101,
+    cat: 'kiji',
     huntable: true,
-    name: 'マガモ',
+    name: 'エゾライチョウ',
     desc: '',
-    src: '/pics/magamo.jpg',
+    src: '/pics/yoshigamo.jpg',
   },
+
+
+
+
   {
-    id: 2,
-    cat: 1,
-    huntable: true,
-    name: 'ヒドリガモ',
-    desc: '',
-    src: '/pics/hidorigamo.jpg',
-  },
-  {
-    id: 3,
-    cat: 1,
-    huntable: true,
-    name: 'オナガガモ',
-    desc: '',
-    src: '/pics/onagagamo.jpg',
-  },
-  {
-    id: 4,
-    cat: 1,
+    id: 101,
+    cat: 'kamo',
     huntable: true,
     name: 'ヨシガモ',
     desc: '',
     src: '/pics/yoshigamo.jpg',
   },
   {
-    id: 5,
-    cat: 1,
+    id: 102,
+    cat: 'kamo',
+    huntable: true,
+    name: 'ヒドリガモ',
+    desc: '',
+    src: '/pics/hidorigamo.jpg',
+  },
+  {
+    id: 103,
+    cat: 'kamo',
+    huntable: true,
+    name: 'マガモ',
+    desc: '',
+    src: '/pics/magamo.jpg',
+  },
+  {
+    id: 104,
+    cat: 'kamo',
     huntable: true,
     name: 'カルガモ',
     desc: '',
     src: '/pics/karugamo.jpg',
   },
   {
-    id: 6,
-    cat: 1,
+    id: 103,
+    cat: 'kamo',
+    huntable: true,
+    name: 'オナガガモ',
+    desc: '',
+    src: '/pics/onagagamo.jpg',
+  },
+  {
+    id: 104,
+    cat: 'kamo',
     huntable: true,
     name: 'ハシビロガモ',
     desc: '',
     src: '/pics/hashibirogamo.jpg',
   },
   {
-    id: 7,
-    cat: 1,
+    id: 105,
+    cat: 'kamo',
+    huntable: true,
+    name: 'オナガガモ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+  {
+    id: 106,
+    cat: 'kamo',
+    huntable: true,
+    name: 'コガモ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+  {
+    id: 107,
+    cat: 'kamo',
+    huntable: true,
+    name: 'ホシハジロ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+  {
+    id: 108,
+    cat: 'kamo',
+    huntable: true,
+    name: 'キンクロハジロ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+  {
+    id: 109,
+    cat: 'kamo',
+    huntable: true,
+    name: 'スズガモ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+  {
+    id: 110,
+    cat: 'kamo',
+    huntable: true,
+    name: 'クロガモ',
+    desc: '',
+    src: '/pics/hashibirogamo.jpg',
+  },
+
+  {
+    id: 301,
+    cat: 'hato',
+    huntable: true,
+    name: 'キジバト',
+    desc: '',
+    src: '/pics/yoshigamo.jpg',
+  },
+
+  {
+    id: 401,
+    cat: 'u',
+    huntable: true,
+    name: 'カワウ',
+    desc: '',
+    src: '/pics/yoshigamo.jpg',
+  },
+
+  {
+    id: 501,
+    cat: 'sagi',
+    huntable: true,
+    name: 'ゴイサギ',
+    desc: '',
+    src: '/pics/yoshigamo.jpg',
+  },
+
+  {
+    id: 601,
+    cat: 'kuina',
     huntable: true,
     name: 'バン',
     desc: '',
     src: '/pics/ban.jpg',
   },
+
+  {
+    id: 701,
+    cat: 'shigi',
+    huntable: true,
+    name: 'ヤマシギ',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+
+  {
+    id: 801,
+    cat: 'karasu',
+    huntable: true,
+    name: 'ミヤマガラス',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+
+  {
+    id: 901,
+    cat: 'hiyodori',
+    huntable: true,
+    name: 'ヒヨドリ',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+
+  {
+    id: 1001,
+    cat: 'mukudori',
+    huntable: true,
+    name: 'ムクドリ',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+
+  {
+    id: 1101,
+    cat: 'suzume',
+    huntable: true,
+    name: 'ニュウナイスズメ',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+  {
+    id: 1102,
+    cat: 'suzume',
+    huntable: true,
+    name: 'スズメ',
+    desc: '',
+    src: '/pics/ban.jpg',
+  },
+
+
+
+
+
+
+
+
+
+
 ]
