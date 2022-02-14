f336563679de085a0750bcb03454991d7008974c
Yoshiyuki Ieyama <yukku0423@gmail.com>
2022-02-14 11:37:51 +0900

    update

--- src/hooks/useDataList.ts
+++ src/hooks/useDataList.ts
@@ -1,17 +1,17 @@
 import DATA from '../resources/data'
 
 interface Query {
-  cat?: number
+  cat?: string
   huntable?: boolean
 }
 
 export function useDataList(query?: Query) {
   let data = DATA
   if(query?.huntable) {
-    data = data.filter(d => d.huntable === query?.huntable)
+    data = data.filter(d => d.huntable === query.huntable)
   }
   if(query?.cat) {
-    data = data.filter(d => d.cat === query?.cat)
+    data = data.filter(d => d.cat === query.cat)
   }
   return {
     data,
